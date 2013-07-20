9650ed3d6867a18b45bf4be7ea10995e15c8adf4
vladi <vlad.ivascau@gmail.com>
2013-07-20 14:59:28 +0100

    Adding clientside code

---
+++ js/modules/huecked.js
@@ -0,0 +1,192 @@
+var app = angular.module('app',['ui.state'])
+
+app.factory('PlayersService', function(){
+
+    var PlayersService = {};
+    var playerList = [];
+
+    PlayersService.clearPlayers = function(){ playerList = [] };
+    PlayersService.getPlayer = function( playerId ){ return playerList[playerId] };
+    PlayersService.getPlayers = function() { return playerList };
+    PlayersService.createPlayers = function(numberOfPlayers){
+        PlayersService.clearPlayers();
+        for(var i = 0; i < numberOfPlayers; i++) {
+            PlayersService.createPlayer();
+        }
+    }
+    PlayersService.createPlayer = function() {
+        var player = {};
+        playerList.push(player);
+        console.log('PlayersService: created new player (id='+playerList.length+')');
+    }
+    PlayersService.resetClickTimers = function(){
+        for(var i = 0; i < playerList.length; i++) {
+            playerList[i].clickTimer = undefined;
+        }
+    }
+
+    return PlayersService;
+
+});
+
+app.factory('GameService', function(){
+
+    var GameService = {};
+
+    GameService.getNewGame = function(){
+
+    }
+
+    return GameService;
+
+});
+
+app.controller('GameController', ['$scope', '$location', '$timeout', 'PlayersService', function ($scope, $location, $timeout, PlayersService) {
+    console.log("GameController");
+
+    // show countdown
+
+    $scope.countdownSeconds = 3;
+    var startCountdown = function(){
+
+        if($scope.countdownSeconds > 0){
+            $timeout( function() { $scope.countdownSeconds--; startCountdown(); }, 1000 );
+        }else if($scope.countdownSeconds == 0){
+            $scope.countdownSeconds = '';
+            $scope.game.currentColor = 'go!'
+            startRound();
+        }
+
+    }
+    startCountdown();
+
+
+    // create players
+
+    PlayersService.createPlayers(2);
+    $scope.players = PlayersService.getPlayers();
+
+    // game options
+
+    var red = 'Red';
+
+    var getColors = function(){
+        return ["Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue"]
+    }
+
+    // game configuration
+
+    var game = {
+
+        'startTime' : undefined,
+        'roundNumber' : 0,
+        'colors' : undefined,
+        'cycle' : 0,
+        'currentColor' : 'ready...',
+        'isRed' : false
+
+    };
+
+    $scope.game = game;
+
+    // start round
+
+    var startRound = function(){
+
+        game.startTime = new Date();
+
+        game.colors = getColors();
+
+        drawColorWithDelay();
+
+    }
+
+    // delay
+    var minMsDelay = 500;
+    var maxMsDelay = 2000;
+
+    var drawColorWithDelay = function(){
+
+        var randomMsDelay = (minMsDelay) + Math.floor(Math.random() * ( (maxMsDelay-minMsDelay) + 1)) + 0;
+
+
+        if(game.currentColor != red){
+            $timeout( function() {
+                game.currentColor = drawColor(game.cycle++, game.colors);
+                drawColorWithDelay();
+            }, randomMsDelay );
+        }else{
+            game.isRed = true;
+        }
+
+    };
+
+    var drawColor = function(cycle, colors){
+
+        if(cycle == 3){
+            addRedColor(colors);
+        }
+
+        console.log('length='+colors.length);
+
+        var randomIndex = Math.floor(Math.random() * (colors.length + 1)) + 0;
+
+        var drawnColor = colors.splice(randomIndex, 1)[0];
+
+        console.log('drawnColor= '+drawnColor +', notDrawn='+colors.toString());
+
+        return drawnColor;
+
+    }
+
+    var addRedColor = function(colors){
+        colors.push(red);
+    }
+
+
+    $scope.click = function(playerId){
+        if(game.isRed){
+            $scope.result = 'Player '+playerId+' wins!';
+
+        }else{
+            $scope.result = 'Player '+playerId+' loses!'
+        }
+        console.log($scope.result);
+    }
+
+}]);
+
+app.controller('IntroController', ['$scope', '$location', '$timeout', function ($scope, $location, $timeout) {
+    console.log("IntroController");
+    $timeout( function(){ $location.path('start') }, 1000);
+}]);
+
+app.config(function($stateProvider, $routeProvider){
+    $stateProvider
+        .state('index', {
+            url: "", // root route
+            views: {
+                "main": {
+                    templateUrl: "js/templates/Intro.html",
+                    controller: 'IntroController'
+                }
+            }
+        })
+        .state('start', {
+            url: "/start",
+            views: {
+                "main": {
+                    templateUrl: "js/templates/Start.html"
+                }
+            }
+        })
+        .state('game', {
+            url: "/game",
+            views: {
+                "main": {
+                    templateUrl: "js/templates/Game.html",
+                    controller: 'GameController'
+                }
+            }
+        })
+})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(80);
+app.listen(8000);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
