af681c10afc1cd8fea089b7b740376652fe8c4fe
Basel Farah <baself@gmail.com>
2013-07-20 15:59:24 +0100

    got basic turn on/off functionality

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+  , hue = require('hue-module')
 
 app.listen(8000);
+
+hue.load("192.168.2.166", "bazathackedio");
+
+var iterateLights = function(state) {
+console.log('iteration');
+	hue.lights(function(lights){
+	    for(i in lights)
+		if(lights.hasOwnProperty(i))
+		    hue.change(lights[i].set({"on": state, "rgb":[0,255,255]}));
+	});
+};
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -18,8 +30,15 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
+  console.log('connected');
   socket.emit('news', { hello: 'world' });
+  socket.on('switchOn', function(data) {
+	iterateLights(data.state);
+  });
+
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
+
+
